caad4b70aec26ae6b802038a0c6ff8d154048e0c
kmoroz <ksmorozova@hotmail.com>
2021-06-13 12:21:00 +0100

    initial commit

---
+++ push_swap.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	stack_a;
+	t_stack	stack_b;
+}
---
+++ push_swap.h
@@ -0,0 +1,9 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stack
+{
+	/* data */
+}				t_stack;
+
+#endif

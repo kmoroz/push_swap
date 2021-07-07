b45de52cdc82e51a2ec3e51cb8eeba72017bd7e4
kmoroz <ksmorozova@hotmail.com>
2021-07-07 14:19:13 +0100

    add more libft functions

---
+++ libft/ft_atol.c
@@ -0,0 +1,27 @@
+long int	ft_atol(const char *str)
+{
+	int			count;
+	long int	strtonum;
+	int			isnegativenum;
+
+	count = 0;
+	strtonum = 0;
+	isnegativenum = 1;
+	while (str[count] == ' ' || str[count] == '\n' || str[count] == '\f'
+		|| str[count] == '\r' || str[count] == '\t' || str[count] == '\v')
+		count++;
+	if (str[count] == '-' || str[count] == '+')
+	{
+		if (str[count] == '-')
+			isnegativenum = -1;
+		count++;
+	}
+	while (str[count] >= '0' && str[count] <= '9')
+	{
+		strtonum = 10 * strtonum + (str[count] - '0');
+		count++;
+	}
+	if (isnegativenum == -1)
+		strtonum = strtonum * -1;
+	return (strtonum);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/10 11:43:49 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/07/06 16:30:03 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/07/07 13:58:04 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ char			**ft_split(char const *str, char dlm);
 size_t			ft_strlen(const char *str);
 char			*ft_strtrim(char const *s1, char const *set);
 size_t			ft_strlcpy(char *dest, const char *src, size_t size);
-int				ft_atoi(const char *str);
 t_node			*ft_lstnew(int content);
+int				ft_isdigit(int argument);
+long int		ft_atol(const char *str);
 
 #endif

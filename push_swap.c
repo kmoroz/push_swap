8365dcaf9a373e5f8d714a283c749e4f01871b99
kmoroz <ksmorozova@hotmail.com>
2021-07-07 14:20:14 +0100

    verify num is less than max int, not a char, etc, count nums on stack

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,7 @@
 #include "push_swap.h"
+#include "limits.h"
 
-void	validate_input(int argc, char **argv)
+void	validate_args(int argc, char **argv)
 {
 	if (argc < 2)
 		ft_error();
@@ -18,14 +19,33 @@ int	calculate_array_size(char **str)
 	return (count);
 }
 
+void	verify_input(const char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+	{
+		if (!ft_isdigit(str[count]))
+			ft_error();
+		count++;
+	}
+	if (ft_strlen(str) > 10)
+		ft_error();
+	if (ft_strlen(str) == 10)
+	{
+		if (ft_atol(str) > INT_MAX)
+			ft_error();
+	}
+}
+
 void	put_num_on_stack(char *str, t_node **head)
 {
 	int		num;
 	t_node	*tail;
 	t_node	*new_node;
 
-	num = ft_atoi(str);
-	//max int protection?
+	num = ft_atol(str);
 	new_node = ft_lstnew(num);
 	if (!*head)
 	{
@@ -45,24 +65,41 @@ void	put_num_on_stack(char *str, t_node **head)
 	}
 }
 
+void	count_stack_size(t_stack *stack_a, t_node **head)
+{
+	t_node	*current;
+	t_node	*tail;
+
+	current = *head;
+	tail = current->prev;
+	while (current != tail)
+	{
+		stack_a->size++;
+		current = current->next;
+	}
+	stack_a->size++;
+}
+
 void	build_stack(int argc, char **argv, t_stack *stack_a)
 {
-	int		count;
+	int		i;
+	int		j;
 	char	**str;
-	int		array_size;
 
-	count = 1;
-	while (count < argc)
+	i = 1;
+	while (i < argc)
 	{
-		str = ft_split(argv[count], ' ');
-		array_size = calculate_array_size(str);
-		while (array_size - 1 >= 0)
+		str = ft_split(argv[i], ' ');
+		j = calculate_array_size(str) - 1;
+		while (j >= 0)
 		{
-			put_num_on_stack(str[array_size - 1], &stack_a->node);
-			array_size--;
+			verify_input(str[j]);
+			put_num_on_stack(str[j], &stack_a->node);
+			j--;
 		}
-		count++;
+		i++;
 	}
+	count_stack_size(stack_a, &stack_a->node);
 }
 
 int	main(int argc, char **argv)
@@ -71,6 +108,7 @@ int	main(int argc, char **argv)
 	t_stack	stack_b;
 
 	stack_a.node = NULL;
-	validate_input(argc, argv);
+	stack_a.size = 0;
+	validate_args(argc, argv);
 	build_stack(argc, argv, &stack_a);
 }

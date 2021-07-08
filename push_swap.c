a87e6b179bc5994afa0b0a147296cb921e250680
kmoroz <ksmorozova@hotmail.com>
2021-07-08 12:38:42 +0100

    check for dupes

--- push_swap.c
+++ push_swap.c
@@ -101,6 +101,38 @@ void	build_stack(int argc, char **argv, t_stack *stack_a)
 	count_stack_size(stack_a, &stack_a->node);
 }
 
+void	make_list_linear(t_node *head)
+{
+	t_node	*tail;
+
+	tail = head->prev;
+	tail->next = NULL;
+}
+
+void	check_dupes(t_node *head)
+{
+	int		count;
+	t_node	*temp;
+	t_node	*temp_2;
+
+	make_list_linear(head);
+	temp = head;
+	while (temp)
+	{
+		count = 0;
+		temp_2 = head;
+		while (temp_2)
+		{
+			if (temp->number == temp_2->number)
+				count++;
+			temp_2 = temp_2->next;
+		}
+		if (count > 1)
+			ft_error();
+		temp = temp->next;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	stack_a;
@@ -110,5 +142,6 @@ int	main(int argc, char **argv)
 	stack_a.size = 0;
 	validate_args(argc, argv);
 	build_stack(argc, argv, &stack_a);
+	check_dupes(stack_a.node);
 	sort_stack(&stack_a, &stack_b);
 }

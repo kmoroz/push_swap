b4e55fbd4bdefdba3f13afd79948dc1b7fab74b2
kmoroz <ksmorozova@hotmail.com>
2021-08-10 17:20:18 +0200

    start implementing checker

---
+++ checker/checker.h
@@ -0,0 +1,22 @@
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "../libft/libft.h"
+# include "../push_swap.h"
+
+typedef enum e_operation
+{
+	SA = 1,
+	SB = 2,
+	SS = 3,
+	PA = 4,
+	PB = 5,
+	RA = 6,
+	RB = 7,
+	RR = 8,
+	RRA = 9,
+	RRB = 10,
+	RRR = 11,
+}	t_operation;
+
+#endif

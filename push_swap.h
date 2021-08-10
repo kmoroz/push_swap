7df1639828f7244d699667d6dc62078f78bd7c56
kmoroz <ksmorozova@hotmail.com>
2021-08-10 17:32:10 +0200

    move enum to checker header

--- push_swap.h
+++ push_swap.h
@@ -3,21 +3,6 @@
 
 # include "libft/libft.h"
 
-typedef enum e_operation
-{
-	SA = 1,
-	SB = 2,
-	SS = 3,
-	PA = 4,
-	PB = 5,
-	RA = 6,
-	RB = 7,
-	RR = 8,
-	RRA = 9,
-	RRB = 10,
-	RRR = 11,
-}	t_operation;
-
 typedef struct s_stack
 {
 	t_node		*node;
@@ -25,7 +10,6 @@ typedef struct s_stack
 	t_node		*partition;
 }				t_stack;
 
-void	ft_error(void);
 void	sort_stack(t_stack *stack_a, t_stack *stack_b);
 void	make_list_linear(t_node *head);
 void	make_list_circular(t_node *head);

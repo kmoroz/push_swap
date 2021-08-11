445229ae280f4d0339ea526ebb72076f75c0b325
Ksenia <ksmorozo@student.codam.nl>
2021-08-11 14:13:50 +0200

    add instructions file

---
+++ checker/instructions.c
@@ -0,0 +1,10 @@
+#include "checker.h"
+
+void	sa_rule(t_stack *stack_a)
+{
+	int	temp;
+
+	temp = stack_a->node->number;
+	stack_a->node->number = stack_a->node->next->number;
+	stack_a->node->next->number = temp;
+}

c49bfccd0278321c817aa9a6538450e88e055c49
kmoroz <ksmorozova@hotmail.com>
2021-09-09 14:46:53 +0100

    expand validate args function

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,15 +6,17 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/09 11:22:02 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/09/09 11:22:07 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/09/09 14:19:05 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	validate_args(int argc)
+void	validate_args(int argc, t_stack *stack_a)
 {
-	if (argc < 2)
+	if (argc == 1)
+		ft_error();
+	else if (stack_a->size == 1)
 		ft_error();
 }
 
@@ -70,8 +72,9 @@ int	main(int argc, char **argv)
 	stack_b.partition = NULL;
 	stack_a.size = 0;
 	stack_b.size = 0;
-	validate_args(argc);
+	validate_args(argc, &stack_a);
 	build_stack(argc, argv, &stack_a);
+	validate_args(argc, &stack_a);
 	check_dupes(stack_a.node);
 	sort_stack(&stack_a, &stack_b);
 }

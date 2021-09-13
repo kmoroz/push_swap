bceadf7190e1269d939998161c778fa89d97a749
kmoroz <ksmorozova@hotmail.com>
2021-09-13 16:49:16 +0100

    get rid of validate args fn

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,23 +6,12 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/09 11:22:02 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/09/13 16:01:25 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/09/13 16:47:39 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
-
-void	validate_args(int argc, t_stack *stack_a)
-{
-	if (argc == 1)
-		ft_error();
-	else if (stack_a->size == 1)
-	{
-		free_node(&stack_a->node, stack_a->size);
-		ft_error();
-	}
-}
 
 int	main(int argc, char **argv)
 {
@@ -33,10 +22,11 @@ int	main(int argc, char **argv)
 	stack_b.node = NULL;
 	stack_a.size = 0;
 	stack_b.size = 0;
-	validate_args(argc, &stack_a);
-	build_stack(argc, argv, &stack_a);
-	validate_args(argc, &stack_a);
-	check_dupes(stack_a.node, stack_a.size);
-	sort_stack(&stack_a, &stack_b);
-	free_node(&stack_a.node, stack_a.size);
+	if (argc > 1)
+	{
+		build_stack(argc, argv, &stack_a);
+		check_dupes(stack_a.node, stack_a.size);
+		sort_stack(&stack_a, &stack_b);
+		free_node(&stack_a.node, stack_a.size);	
+	}
 }

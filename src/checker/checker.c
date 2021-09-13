4f1e54f45fbfb7e84448dfc6ff237b35e6e3463c
kmoroz <ksmorozova@hotmail.com>
2021-09-13 16:29:43 +0100

    optimise checker against leaks, etc

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/13 13:42:35 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/09/13 14:00:43 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/09/13 16:20:38 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	record_instruction(char *line, t_node **head)
+void	handle_invalid_instruction(t_stack *instructions, t_stack *stack_a)
 {
-	if (!ft_strncmp("sa", line, ft_strlen(line)))
-		put_num_on_stack(SA, head);
-	else if (!ft_strncmp("sb", line, ft_strlen(line)))
-		put_num_on_stack(SB, head);
-	else if (!ft_strncmp("ss", line, ft_strlen(line)))
-		put_num_on_stack(SS, head);
-	else if (!ft_strncmp("pa", line, ft_strlen(line)))
-		put_num_on_stack(PA, head);
-	else if (!ft_strncmp("pb", line, ft_strlen(line)))
-		put_num_on_stack(PB, head);
-	else if (!ft_strncmp("ra", line, ft_strlen(line)))
-		put_num_on_stack(RA, head);
-	else if (!ft_strncmp("rb", line, ft_strlen(line)))
-		put_num_on_stack(RB, head);
-	else if (!ft_strncmp("rr", line, ft_strlen(line)))
-		put_num_on_stack(RR, head);
-	else if (!ft_strncmp("rra", line, ft_strlen(line)))
-		put_num_on_stack(RRA, head);
-	else if (!ft_strncmp("rrb", line, ft_strlen(line)))
-		put_num_on_stack(RRB, head);
-	else if (!ft_strncmp("rrr", line, ft_strlen(line)))
-		put_num_on_stack(RRR, head);
-	else
-		ft_error();
+	free_node(&stack_a->node, stack_a->size);
+	free_node(&instructions->node, instructions->size);
+	ft_error();
 }
 
-void	get_instructions(t_node	**instructions)
+void	record_instruction(char *line, t_stack *instructions, t_stack *stack_a)
+{
+	if (!ft_strncmp("sa", line, ft_strlen(line)))
+		put_num_on_stack(SA, &instructions->node);
+	else if (!ft_strncmp("sb", line, ft_strlen(line)))
+		put_num_on_stack(SB, &instructions->node);
+	else if (!ft_strncmp("ss", line, ft_strlen(line)))
+		put_num_on_stack(SS, &instructions->node);
+	else if (!ft_strncmp("pa", line, ft_strlen(line)))
+		put_num_on_stack(PA, &instructions->node);
+	else if (!ft_strncmp("pb", line, ft_strlen(line)))
+		put_num_on_stack(PB, &instructions->node);
+	else if (!ft_strncmp("ra", line, ft_strlen(line)))
+		put_num_on_stack(RA, &instructions->node);
+	else if (!ft_strncmp("rb", line, ft_strlen(line)))
+		put_num_on_stack(RB, &instructions->node);
+	else if (!ft_strncmp("rr", line, ft_strlen(line)))
+		put_num_on_stack(RR, &instructions->node);
+	else if (!ft_strncmp("rra", line, ft_strlen(line)))
+		put_num_on_stack(RRA, &instructions->node);
+	else if (!ft_strncmp("rrb", line, ft_strlen(line)))
+		put_num_on_stack(RRB, &instructions->node);
+	else if (!ft_strncmp("rrr", line, ft_strlen(line)))
+		put_num_on_stack(RRR, &instructions->node);
+	else
+		handle_invalid_instruction(instructions, stack_a);
+}
+
+void	get_instructions(t_stack *stack_a, t_stack *instructions)
 {
 	int		status;
 	char	*line;
@@ -53,36 +60,37 @@ void	get_instructions(t_node	**instructions)
 	{
 		status = get_next_line(STDIN_FILENO, &line);
 		if (*line)
-			record_instruction(line, instructions);
+			record_instruction(line, instructions, stack_a);
 		free(line);
 	}
+}
+
+void	display_checker_status(t_stack *stack_a, t_stack *stack_b)
+{
+	if (is_sorted(stack_a->node) && !stack_b->size)
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
 }
 
 int	main(int argc, char **argv)
 {
 	t_stack	stack_a;
-	t_node	*instructions;
+	t_stack	instructions;
 	t_stack	stack_b;
 
-	stack_a.node = NULL;
-	stack_a.size = 0;
-	stack_b.node = NULL;
-	stack_b.size = 0;
-	instructions = NULL;
+	init_checker_structs(&stack_a, &stack_b, &instructions);
 	if (argc > 1)
 	{
 		build_stack(argc, argv, &stack_a);
 		if (is_sorted(stack_a.node))
-			exit(0);
-		check_dupes(stack_a.node);
-		get_instructions(&instructions);
-		if (instructions)
-			apply_instructions(instructions, &stack_a, &stack_b);
-		if (is_sorted(stack_a.node) && !stack_b.size)
-			write(1, "OK\n", 3);
-		else
-			write(1, "KO\n", 3);
+			exit(free_node(&stack_a.node, stack_a.size));
+		check_dupes(stack_a.node, stack_a.size);
+		get_instructions(&stack_a, &instructions);
+		count_stack_size(&instructions, &instructions.node);
+		apply_instructions(instructions.node, &stack_a, &stack_b);
+		display_checker_status(&stack_a, &stack_b);
+		free_node(&stack_a.node, stack_a.size);
+		free_node(&instructions.node, instructions.size);
 	}
-	else
-		exit(0);
 }
